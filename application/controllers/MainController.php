6a14660a0922bc68bd61a8c2198f8454997894dd
Leonid Dobryvechir <leo.dobrivecher@gmail.com>
2018-08-06 16:33:53 +0300

    init proj

---
+++ application/controllers/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace application\controllers;
+
+use application\core\Controller;
+
+class MainController extends Controller
+{
+    
+    public function indexAction()
+    {
+        $result = $this->model->getNews();
+        $this->view->render('Главная страница', ['news' => $result,]);
+    }
+
+}
